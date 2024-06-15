32ca48db814dddede6724a6f7bf49d115596ffee
DanielDevAmaral <daniel.amaral1928@outlook.com>
2024-06-15 16:01:16 -0300

    w1 final

---
+++ scripts/getdates.js
@@ -0,0 +1,6 @@
+const year = document.querySelector("#currentyear");
+const lastMod = document.querySelector("#lastModified");
+let oLastModif = new Date(document.lastModified);
+const today = new Date();
+lastMod.innerHTML =`${oLastModif}`
+year.innerHTML = `&copy${today.getFullYear()}`;
